7dd58c9c84f3b602fc2dd396a47c60dd33fa4f04
jfarrellhfx <jfarrellhfx@gmail.com>
2022-04-25 16:10:57 -0600

    finished code, needs documentation

---
+++ arxiv-importer.py
@@ -0,0 +1,132 @@
+print("\033c")
+import arxiv
+from datetime import datetime, timezone
+import logging
+import string
+
+logging.basicConfig(level=logging.INFO)
+
+categories = [
+    "cond-mat.mes-hall",
+    "cond-mat.str-el",
+    "cond-mat.stat-mech",
+    "cond-mat.supr-con"
+]
+
+keywords = [
+    'hydrodynamic',
+    '"effective field theory"',
+    'kinetic theory',
+]
+
+authors = [
+    "Nandkishore",
+    "Lucas",
+    "Scaffidi",
+    ]
+
+def makelist(list):
+    query = ""
+    for item in list:
+        query = query+ " " + item
+    return query
+
+
+search_authors = arxiv.Search(
+    query = "cat:({}) AND (au:({}))".format(
+        makelist(categories),
+        makelist(authors)
+    ),
+    sort_by=arxiv.SortCriterion.SubmittedDate
+    )
+
+search_keywords = arxiv.Search(
+    query = "cat:({}) AND (all:({}))".format(
+        makelist(categories),
+        makelist(keywords)
+    ),
+    sort_by=arxiv.SortCriterion.SubmittedDate
+    )
+
+
+now = datetime.now(timezone.utc)
+
+papers = []
+with open("test.html", "w") as f:
+
+    with open("head.html","r") as g:
+        for line in g.readlines():
+            f.write(line + "\n")
+
+    # authors
+    f.write("<h2>Followed Authors:</h2>\n")
+    for result in search_authors.results():
+        diff = now - result.published
+        hrs = diff.total_seconds()/3600
+        
+        # Stop if the 
+        if hrs > 80:
+            break
+
+        print(result.title)
+        print(result.published.date())
+        print("")
+
+        
+        f.write("<body>\n")
+        f.write("<h3>{}</h3>\n".format(result.title))
+        f.write("<a href = \"{}\">{}</a>\n<br>\n".format(result.links[0], result.links[0]))
+
+        authors = ""
+        for author in result.authors:
+            name = author.name
+            authors = authors + ", " + name.encode("ascii", errors = "ignore").decode()
+            if authors[0] == ",":
+                authors = authors[2:]
+
+        f.write("<i>{}</i>\n".format(authors))
+        f.write("<br>\n")
+        f.write("<br>\n")
+        f.write("{}\n".format(result.summary))
+        f.write("<br>\n")
+        f.write("<br>\n")
+
+    # Keywords
+    f.write("<h2>Keywords:</h2>\n")
+    for result in search_keywords.results():
+        diff = now - result.published
+        hrs = diff.total_seconds()/3600
+        
+        # Stop if the 
+        if hrs > 80:
+            break
+
+        print(result.title)
+        print(result.published.date())
+        print("")
+
+        
+        f.write("<body>\n")
+        f.write("<h3>{}</h3>\n".format(result.title))
+        f.write("<a href = \"{}\">{}</a>\n<br>\n".format(result.links[0], result.links[0]))
+
+        authors = ""
+        for author in result.authors:
+            name = author.name
+            authors = authors + ", " + name.encode("ascii", errors = "ignore").decode()
+            if authors[0] == ",":
+                authors = authors[2:]
+
+        f.write("<i>{}</i>\n".format(authors))
+        f.write("<br>\n")
+        f.write("<br>\n")
+        f.write("{}\n".format(result.summary))
+        f.write("<br>\n")
+        f.write("<br>\n")
+        f.write("<br>\n")
+        f.write("<br>\n")
+        
+        f.write("</body>")
+
+   
+    
