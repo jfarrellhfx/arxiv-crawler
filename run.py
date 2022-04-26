88864f6c437ff6cf991a7117d84829146216609e
jfarrellhx <jfarrellhfx@gmail.com>
2022-04-26 02:44:40 +0000

    new day

--- run.py
+++ run.py
@@ -18,4 +18,5 @@ os.system("git add .")
 os.system('git config --global user.name "jfarrellhx"')
 os.system('git config --global user.email "jfarrellhfx@gmail.com"')
 os.system('git commit -m "new day"')
-os.system('git push https://jfarrellhfx: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@github.com/jfarrellhfx/arxiv-crawler.git main ')
+os.system('git push https://jfarrellhfx:Weasel2089!g@github.com/jfarrellhfx/arxiv-crawler.git main')
+#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
