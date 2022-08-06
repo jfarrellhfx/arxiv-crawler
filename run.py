f391003109b99a4d431a1b988d11a2da7d16aa36
jfarrellhfx <jfarrellhfx@gmail.com>
2022-08-06 01:52:27 -0300

    fix

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ yesterday = datetime.now() - timedelta(hours = 24)
 yesterday = str(yesterday.date())
 
 # move the previous index.html to the archive folder
-os.system("mv index.html {}-papers.html".format(yesterday))
+os.system("mv index.html archive/{}-papers.html".format(yesterday))
 
 sleep(1) # wait a second
 
@@ -33,15 +33,7 @@ with open("Archive.html", "w") as f:
 # create the new list of papers
 os.system("python arxiv-importer.py")
 
-# github access token: needs to be broken up, because github doesn't like you to put the tokens inside of the repositories!
-#
-token1 = "ghp_Gomuk1hkXJLN9"
-#
-token2 = "Ik3mzWZu4YMEzuwSb4aM965"
-
 # update the website
 os.system("git add .")
-os.system('git config --global user.name "jfarrellhx"')
-os.system('git config --global user.email "jfarrellhfx@gmail.com"')
 os.system('git commit -m "new day"')
-os.system('git push https://jfarrellhfx:{}@github.com/jfarrellhfx/arxiv-crawler.git main'.format(token))
+os.system('git push origin main')
