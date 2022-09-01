bf27ffa242eddeebc4716ef396715d6317469f8b
jfarrellhfx <jfarrellhfx@gmail.com>
2022-08-31 19:54:10 -0600

    fix

--- arxiv-importer.py
+++ arxiv-importer.py
@@ -8,7 +8,7 @@ Using Arxiv API to search for papers with certain categories, authors, and keywo
 print("\033c")
 import arxiv
 import os
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 import logging
 import string
 
@@ -37,12 +37,9 @@ authors = [
     '"Andrew Lucas"',
     '"Thomas Scaffidi"',
     '"Paolo Glorioso"',
-    '"Sean Hartnoll"',
-    '"Ananth Kandala"'
+    '"Sean Hartnoll"'
     ]
 
-# keep track of whether there are any results
-good_results = 0
 
 # period
 period = 24
@@ -81,11 +78,62 @@ search_keywords = arxiv.Search(
         makelist(categories),
         makelist(keywords)
     ),
-    sort_by=arxiv.SortCriterion.SubmittedDate
+    sort_by=arxiv.SortCriterion.SubmittedDate,
     )
 
+
+
+
 # the time
-now = datetime.now(timezone.utc)
+now_utc = datetime(year=2022,month=8,day=31,hour=22,minute=5).replace(tzinfo=timezone.utc)
+now_est = now_utc - timedelta(hours = 4)
+print("time")
+
+
+def good_results(allresults):
+    goodresults = []
+
+    # what to do on tuesday, wednesday, thursday
+    if now_est.weekday() in [1,2,3]:
+        print("weekday")
+       
+        for result in allresults:
+            print(result.published.tzinfo)
+            print("")
+            diff = now_utc- result.published
+            hrs = diff.total_seconds() / 3600
+            if hrs > 2 and hrs < 30:
+                goodresults.append(result)
+                 
+            if hrs > 100:
+                break
+
+    elif now_est.weekday() == 6:
+        print("SUNDAY!!")
+        for result in allresults:
+            diff = now_utc- result.published
+            hrs = diff.total_seconds() / 3600
+            if hrs > 50 and hrs < 78:
+                goodresults.append(result)
+            if hrs > 100:
+                break
+    elif now_est.weekday() == 0:
+        print("monday")
+        for result in allresults:
+            diff = now_utc- result.published
+          
+            hrs = diff.total_seconds() / 3600
+            if hrs > 2 and hrs < 78:
+                goodresults.append(result)
+            if hrs > 100:
+                break
+    return goodresults
+
+
+
+        
+
+
 
 # create a html file to which to put the results
 with open("index.html", "w") as f:
@@ -94,111 +142,100 @@ with open("index.html", "w") as f:
     with open("head.html","r") as g:
         for line in g.readlines():
             f.write(line + "\n")
+        f.write("<body>\n")
 
-    # authors ---------------------------------------------------
+
+   
+
+        
+
+
+
+        # authors ---------------------------------------------------
 
     # title
     f.write("<h2>Followed Authors:</h2>\n")
+    try:
+        # iterate through results
+        for result in good_results(search_authors.results()):
+            
+            
+            # format everyhthing nicely with links
+            print(result.title)
+            
+            f.write("<h3>{}</h3>\n".format(result.title))
+            f.write("<a href = \"{}\",target=\"_blank\">{}</a>\n<br>\n".format(result.links[0], result.links[0]))
 
-    # iterate through results
-    for result in search_authors.results():
+            authors = ""
+            for author in result.authors:
+                name = author.name
+                authors = authors + ", " + name.encode("ascii", errors = "ignore").decode()
+                if authors[0] == ",":
+                    authors = authors[2:]
 
-        # figure out how long it's been since the result was published, and break the loop if this time is greater than 24 hrs.
-        diff = now - result.published
-        hrs = diff.total_seconds()/3600
-        if hrs > period:
-            break
-
-        # format everyhthing nicely with links
-        print(result.title)
-        print(result.published.date())
-        print("")
-        good_results += 1
-
-
-        f.write("<body>\n")
-        f.write("<h3>{}</h3>\n".format(result.title))
-        f.write("<a href = \"{}\">{}</a>\n<br>\n".format(result.links[0], result.links[0]))
-
-        authors = ""
-        for author in result.authors:
-            name = author.name
-            authors = authors + ", " + name.encode("ascii", errors = "ignore").decode()
-            if authors[0] == ",":
-                authors = authors[2:]
-
-        f.write("<i>{}</i>\n".format(authors))
-        f.write("<br>\n")
-        f.write("<br>\n")
-        f.write("{}\n".format(result.summary))
-        f.write("<br>\n")
-        f.write("<br>\n")
+            f.write("<i>{}</i>\n".format(authors))
+            f.write("<br>\n")
+            f.write("{}\n".format(result.summary))
+            f.write("<br>\n")
+            f.write("<br>\n")
+    except: pass
 
     # Keywords ------------------------------------------------------------------
     # ditto everything from the authors...
-    f.write("<h2>Keywords:</h2>\n")
-    for result in search_keywords.results():
-        diff = now - result.published
-        hrs = diff.total_seconds()/3600
+    try:
+        f.write("<h2>Keywords:</h2>\n")
+        for result in good_results(search_keywords.results()):
+            
 
-        # Stop if the
-        if hrs > period:
-            break
+            print(result.title)
+            print(result.published)
+            print("")
+            f.write("<h3>{}</h3>\n".format(result.title))
+            f.write("<a href = \"{}\",target=\"_blank\">{}</a>\n<br>\n".format(result.links[0], result.links[0]))
 
-        print(result.title)
-        print(result.published.date())
-        print("")
-        good_results += 1
-        f.write("<h3>{}</h3>\n".format(result.title))
-        f.write("<a href = \"{}\">{}</a>\n<br>\n".format(result.links[0], result.links[0]))
+            authors = ""
+            for author in result.authors:
+                name = author.name
+                authors = authors + ", " + name.encode("ascii", errors = "ignore").decode()
+                if authors[0] == ",":
+                    authors = authors[2:]
 
-        authors = ""
-        for author in result.authors:
-            name = author.name
-            authors = authors + ", " + name.encode("ascii", errors = "ignore").decode()
-            if authors[0] == ",":
-                authors = authors[2:]
-
-        f.write("<i>{}</i>\n".format(authors))
-        f.write("<br>\n")
-        f.write("<br>\n")
-        f.write("{}\n".format(result.summary))
-        f.write("<br>\n")
-        f.write("<br>\n")
+            f.write("<i>{}</i>\n".format(authors))
+            f.write("<br>\n")
+            f.write("{}\n".format(result.summary))
+            f.write("<br>\n")
+            f.write("<br>\n")
+    except: pass
 
 
 
     f.write("<h2>All Papers:</h2>\n")
-    for result in search_all.results():
-        diff = now - result.published
-        hrs = diff.total_seconds()/3600
+    try:
+        for result in good_results(search_all.results()):
+            print(result.published.time())
+            print(result.title)
+            print(result.published.date())
+            print("")
 
-        # Stop if the
-        if hrs > period:
-            break
 
-        print(result.title)
-        print(result.published.date())
-        print("")
-        good_results += 1
+            f.write("<h3>{}</h3>\n".format(result.title))
+            f.write("<a href = \"{}\",target=\"_blank\">{}</a>\n<br>\n".format(result.links[0], result.links[0]))
 
-        f.write("<h3>{}</h3>\n".format(result.title))
-        f.write("<a href = \"{}\">{}</a>\n<br>\n".format(result.links[0], result.links[0]))
+            authors = ""
+            for author in result.authors:
+                name = author.name
+                authors = authors + ", " + name.encode("ascii", errors = "ignore").decode()
+                if authors[0] == ",":
+                    authors = authors[2:]
 
-        authors = ""
-        for author in result.authors:
-            name = author.name
-            authors = authors + ", " + name.encode("ascii", errors = "ignore").decode()
-            if authors[0] == ",":
-                authors = authors[2:]
+            f.write("<i>{}</i>\n".format(authors))
+            f.write("<br>\n")
 
-        f.write("<i>{}</i>\n".format(authors))
-        f.write("<br>\n")
-        f.write("<br>\n")
-        f.write("{}\n".format(result.summary))
-        f.write("<br>\n")
-        f.write("<br>\n")
+            f.write("{}\n".format(result.summary))
+            f.write("<br>\n")
+            f.write("<br>\n")
+    except: pass
 
-        f.write("</body>")
+    f.write("</body>")
 
 
--- run.py
+++ run.py
@@ -1,34 +1,43 @@
 # imports
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from time import sleep
 
 # today and yesterday's date as strings
-today = str(datetime.now().date())
-yesterday = datetime.now() - timedelta(hours = 24)
+today = str(datetime.now(timezone(timedelta(hours=-5), 'EST')).date())
+
+if datetime.now(timezone(timedelta(hours=-5), 'EST')).weekday() in [0,1,2,3,4,5]:
+    yesterday = datetime.now(timezone(timedelta(hours=-5), 'EST')) - timedelta(hours = 24)
+elif datetime.now(timezone(timedelta(hours=-5), 'EST')).weekday() == 6:
+    yesterday = datetime.now(timezone(timedelta(hours=-5), 'EST')) - timedelta(hours = 24 * 3)
+
+
 yesterday = str(yesterday.date())
 
-# move the previous index.html to the archive folder
-os.system("mv index.html archive/{}-papers.html".format(yesterday))
-
-sleep(1) # wait a second
-
-# Open an html file to hold links to all the old results.
-with open("Archive.html", "w") as f:
-
-    # add the header
-    with open("head.html","r") as g:
-        for line in g.readlines():
-            f.write(line + "\n")
-
-    # for every filename that starts with a sensible date in the 21st century, add a link to the corresponding page in the arxiv file.
-    for name in sorted(os.listdir("archive"))[::-1]:
-        if name[0] == "2":
-            name2 = name[:10]
-            f.write('<a href = "archive/{}">{}</a>\n'.format(name, name2))
-            f.write("<br>\n")
-    f.write("</body>")
+if datetime.now(timezone(timedelta(hours=-5), 'EST')).weekday() in [0,1,2,3,6]:
 
 
-# create the new list of papers
-os.system("python arxiv-importer.py")
+    # move the previous index.html to the archive folder
+    os.system("mv index.html archive/{}-papers.html".format(yesterday))
+
+    sleep(1) # wait a second
+
+    # Open an html file to hold links to all the old results.
+    with open("Archive.html", "w") as f:
+
+        # add the header
+        with open("head.html","r") as g:
+            for line in g.readlines():
+                f.write(line +"\n")
+
+        # for every filename that starts with a sensible date in the 21st century, add a link to the corresponding page in the arxiv file.
+        for name in sorted(os.listdir("archive"))[::-1]:
+            if name[0] == "2":
+                name2 = name[:10]
+                f.write('<a href = "archive/{}">{}</a>\n'.format(name, name2))
+                f.write("<br>\n")
+        f.write("</body>")
+
+
+    # create the new list of papers
+    os.system("python arxiv-importer.py")
